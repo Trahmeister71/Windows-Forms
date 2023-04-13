10796835c6420e36783d0eeb23a380a906fc25c5
Trahmeister71 <nikita.lichkun03@gmail.com>
2023-04-14 01:09:07 +0300

    TabControl

---
+++ TAbControl/TAbControl/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace TAbControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.radioButton9 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabPage1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox3);
+            this.tabPage1.Controls.Add(this.groupBox2);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(451, 276);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.radioButton7);
+            this.groupBox3.Controls.Add(this.radioButton8);
+            this.groupBox3.Controls.Add(this.radioButton9);
+            this.groupBox3.Location = new System.Drawing.Point(243, 3);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 100);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "3)Какой из динозавров был летающим?";
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(6, 77);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(98, 17);
+            this.radioButton7.TabIndex = 2;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "Пахиринозавр";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(6, 54);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(87, 17);
+            this.radioButton8.TabIndex = 1;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "Плезиозавр";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // radioButton9
+            // 
+            this.radioButton9.AutoSize = true;
+            this.radioButton9.Location = new System.Drawing.Point(6, 31);
+            this.radioButton9.Name = "radioButton9";
+            this.radioButton9.Size = new System.Drawing.Size(102, 17);
+            this.radioButton9.TabIndex = 0;
+            this.radioButton9.TabStop = true;
+            this.radioButton9.Text = "Кетцалькоатль";
+            this.radioButton9.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Controls.Add(this.radioButton5);
+            this.groupBox2.Controls.Add(this.radioButton6);
+            this.groupBox2.Location = new System.Drawing.Point(3, 122);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "12)Кто открыл Америку?";
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(17, 62);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(43, 17);
+            this.radioButton4.TabIndex = 2;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Кук";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(15, 39);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(81, 17);
+            this.radioButton5.TabIndex = 1;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Христофор";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(15, 16);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(69, 17);
+            this.radioButton6.TabIndex = 0;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "Магелан";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(3, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "1)Как называли бога войны в Риме?";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(8, 54);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(52, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Марс";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(8, 31);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(50, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Арес";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(8, 77);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(38, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Ра";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(459, 302);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 320);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Добавить следуйщую вкладку";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(284, 320);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(163, 58);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Вывести результат";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 398);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabPage1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.RadioButton radioButton9;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ TAbControl/TAbControl/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TAbControl
+{
+    public partial class Form1 : Form
+    {
+        int countcorrect=0;
+        RadioButton radioButton3_2;
+        RadioButton radioButton2_3;
+        RadioButton radioButton1_1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GroupBox groupBox1 = new GroupBox();
+            groupBox1.Text = "4)Столица Малайзии";
+            groupBox1.Location = new Point(10, 10);
+            groupBox1.Size = new Size(200, 100);
+
+            // создание RadioButton-ов для первого GroupBox
+            radioButton1_1 = new RadioButton();
+            radioButton1_1.Text = "Куала-лумпур";
+            radioButton1_1.Location = new Point(20, 20);
+            radioButton1_1.Size = new Size(150, 20);
+
+            RadioButton radioButton1_2 = new RadioButton();
+            radioButton1_2.Text = "Бангладеш";
+            radioButton1_2.Location = new Point(20, 40);
+            radioButton1_2.Size = new Size(150, 20);
+
+            RadioButton radioButton1_3 = new RadioButton();
+            radioButton1_3.Text = "Лаос";
+            radioButton1_3.Location = new Point(20, 60);
+            radioButton1_3.Size = new Size(150, 20);
+
+            // добавление RadioButton-ов в первый GroupBox
+            groupBox1.Controls.Add(radioButton1_1);
+            groupBox1.Controls.Add(radioButton1_2);
+            groupBox1.Controls.Add(radioButton1_3);
+
+            // создание второго GroupBox
+            GroupBox groupBox2 = new GroupBox();
+            groupBox2.Text = "5)Сколько людей убила испанка";
+            groupBox2.Location = new Point(10, 120);
+            groupBox2.Size = new Size(200, 100);
+
+            // создание RadioButton-ов для второго GroupBox
+            RadioButton radioButton2_1 = new RadioButton();
+            radioButton2_1.Text = "9 млн";
+            radioButton2_1.Location = new Point(20, 20);
+            radioButton2_1.Size = new Size(150, 20);
+
+            RadioButton radioButton2_2 = new RadioButton();
+            radioButton2_2.Text = "17 млн";
+            radioButton2_2.Location = new Point(20, 40);
+            radioButton2_2.Size = new Size(150, 20);
+
+            radioButton2_3 = new RadioButton();
+            radioButton2_3.Text = "12,5 млн";
+            radioButton2_3.Location = new Point(20, 60);
+            radioButton2_3.Size = new Size(150, 20);
+
+            // добавление RadioButton-ов во второй GroupBox
+            groupBox2.Controls.Add(radioButton2_1);
+            groupBox2.Controls.Add(radioButton2_2);
+            groupBox2.Controls.Add(radioButton2_3);
+
+            // создание третьего GroupBox
+            GroupBox groupBox3 = new GroupBox();
+            groupBox3.Text = "Кто написал TheFacebook";
+            groupBox3.Location = new Point(235, 10);
+            groupBox3.Size = new Size(200, 100);
+
+            // создание RadioButton-ов для третьего GroupBox
+            RadioButton radioButton3_1 = new RadioButton();
+            radioButton3_1.Text = "Стив Джобс";
+            radioButton3_1.Location = new Point(20, 20);
+            radioButton3_1.Size = new Size(150, 20);
+
+            radioButton3_2 = new RadioButton();
+            radioButton3_2.Text = "Марк Цукерберг";
+            radioButton3_2.Location = new Point(20, 40);
+            radioButton3_2.Size = new Size(150, 20);
+
+            RadioButton radioButton3_3 = new RadioButton();
+            radioButton3_3.Text = "Билл Гейтс";
+            radioButton3_3.Location = new Point(20, 60);
+            radioButton3_3.Size = new Size(150, 20);
+
+            groupBox3.Controls.Add(radioButton3_1);
+            groupBox3.Controls.Add(radioButton3_2);
+            groupBox3.Controls.Add(radioButton3_3);
+
+            TabPage tabpage2 = new TabPage("2");
+            tabpage2.Controls.Add(groupBox1);
+            tabpage2.Controls.Add(groupBox2);
+            tabpage2.Controls.Add(groupBox3);
+            tabControl1.TabPages.Add(tabpage2);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {            
+            if (radioButton3.Checked)
+                countcorrect++;
+            if (radioButton5.Checked)
+                countcorrect++;
+            if (radioButton9.Checked)
+                countcorrect++;
+            if (radioButton1_1.Checked)
+                countcorrect++;
+            if (radioButton2_3.Checked)
+                countcorrect++;
+            if (radioButton3_2.Checked)
+                countcorrect++;
+            MessageBox.Show($"Правильные ответы: {countcorrect}");
+            countcorrect = 0;
+        }
+    }
+}
